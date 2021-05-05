e689f67195d03d3a076208af36b446a44fddffcd
tripleslicer <williamrevans@gmail.com>
2021-05-04 19:16:31 -0700

    add pictures and alt text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img2 = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="false">
@@ -9,15 +11,23 @@ ReactDOM.render(
     <div>
       <img
         className="food-img"
+        alt="taco town"
         src="https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1502767705602-P34YV2BMBOWRXOKB7Z7W/ke17ZwdGBToddI8pDm48kKDKtHioz13A6gTD6cP2SJZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0ooWhOa5cxQSJsU3rXf8luX33ZLveKUc0IfIDXzCtWBDa4GLkaq2lSHIaS7p2YxCXg/tacotown.jpg?format=2500w"
       ></img>
       <img
         className="food-img"
+        alt="wilson farms"
         src="https://bloximages.chicago2.vip.townnews.com/niagara-gazette.com/content/tncms/assets/v3/editorial/f/0b/f0b3e6f3-04ef-54d3-9821-c0bd0f78470a/53ed02ad06403.preview-620.jpg?crop=620%2C349%2C0%2C33&resize=1200%2C675&order=crop%2Cresize"
       ></img>
       <img
         className="food-img"
+        alt="old pickles"
         src="https://cdn.cdkitchen.com/recipes/images/2016/05/5209-6233-mx.jpg"
+      ></img>
+      <img
+        className="food-img"
+        src={img2 + "?grayscale"}
+        alt="random picture"
       ></img>
     </div>
   </div>,
